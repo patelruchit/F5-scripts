0982d7e6a56b2f46a276d49e97043df81ef19ee5
Ruchit <40352317+patelruchit@users.noreply.github.com>
2020-10-01 04:09:57 +0530

    Update F5_ltm_pool.sh

--- LTM/F5_ltm_pool.sh
+++ LTM/F5_ltm_pool.sh
@@ -11,7 +11,7 @@ read -r NAME
 for i in `seq 1 $TOTAL_COUNT`
 do 
 #Make changes to the F5 command as required
-    echo "Creating Pool with name $NAME$i and one member endurance_node$i:80"  
+    echo "Creating Pool with name $NAME$i and one member test_node$i:80"  
 #Note that the nodes were created already and the naming sequence is used here to utilize to add them to the pool.
     tmsh create ltm pool $NAME$i members add { test_node$i:80 { monitor gateway_icmp } }
     ((START=START+1))
