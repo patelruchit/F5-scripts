6e93ea2f351dfc01ce3b452ee0adac6bad0c0e87
Ruchit <40352317+patelruchit@users.noreply.github.com>
2021-02-25 15:20:17 +0530

    Update F5_ltm_node.sh

--- LTM/F5_ltm_node.sh
+++ LTM/F5_ltm_node.sh
@@ -12,15 +12,27 @@ read -r FIRST_IP
 echo -e "Enter the name you want for the Pool members:\c"
 read -r NAME
 
-#Function to convert the IP to integer for ease of change of subnets
-dotted_decimal_to_integer()
+#Function to convert the IP to integer
+function iptoint
 {
-#using dot '.' as the internal field separator variable (IFS) as it is an IP.
-    IFS="." read a b c d <<< `echo $1`
+#Returns the integer representation of an IP arg, passed in ascii dotted-decimal notation (x.x.x.x)
+IP=$1; IPNUM=0
+for (( i=0 ; i<4 ; ++i )); do
+((IPNUM+=${IP%%.*}*$((256**$((3-${i}))))))
+IP=${IP#*.}
+done
+echo $IPNUM 
+} 
 
+#Function to convert the IP to integer for ease of change of subnets
+#dotted_decimal_to_integer()
+#{
+#using dot '.' as the internal field separator variable (IFS) as it is an IP.
+#    IFS="." read a b c d <<< `echo $1`
+#
  # Left-shift the values as per bits position for the octects.   
-    expr $(( (a<<24) + (b<<16) + (c<<8) + d ))
-}
+#    expr $(( (a<<24) + (b<<16) + (c<<8) + d ))
+#}
 
 #Function to convert integer to IP for F5 and echo
 integer_to_dotted_decimal()
@@ -33,7 +45,7 @@ integer_to_dotted_decimal()
     echo "${a}.${b}.${c}.${d}"
 }
 #Convert the first ip to integer 
-START=$(dotted_decimal_to_integer $FIRST_IP)
+START=$(iptoint $FIRST_IP)
 
 for i in `seq 1 $TOTAL_COUNT`
 do 
